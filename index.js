359a72a6cec93a982e11927c28e361afdb59b33d
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-09 22:36:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+     const textElement = document.getElementById('text');
+    
+   
+    textElement.textContent = 'This is really cool!';
+});
+
